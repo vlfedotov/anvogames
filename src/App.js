38578a0b56e0b6b96411b170dd9ea1baf334f7d1
vlfedotov <fedotov.vu@gmail.com>
2017-06-06 16:24:48 +0300

    add asteroids link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import asteroids_logo from '../games/asteroids/asteroids_logo.png'
 
 class App extends Component {
   render() {
@@ -10,6 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>AnVo Games</h2>
           <h3>v0.1</h3>
+        </div>
+        <div>
+          <a href='../games/asteroids/index.html'><img src={asteroids_logo} width=64 height=64></img></a>
         </div>
       </div>
     );
