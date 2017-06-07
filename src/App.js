75acce665875f523546aaecfb86ebcb5c40432ba
vlfedotov <fedotov.vu@gmail.com>
2017-06-07 23:09:34 +0300

    add navBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import asteroids_logo from './games/asteroids/asteroids_logo.png';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.css';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div className="App">
+        <Navbar color="faded" light toggleable>
+          <NavbarToggler right onClick={this.toggle} />
+          <NavbarBrand href="/">AnVo Games</NavbarBrand>
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">Github</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>AnVo Games</h2>
